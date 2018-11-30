843b9e2a76979311a30c375986d4179b99e370da
Emil Møller Rasmussen <35135818+hy42-tmf@users.noreply.github.com>
2018-11-30 14:16:17 +0100

    Create rasterio_mask.py

---
+++ rasterio_mask.py
@@ -0,0 +1,15 @@
+import fiona, rasterio, gdal
+from osgeo import osr
+
+def clipper(InputTIF, InputSHP, OutTIF):
+    with fiona.open(InputSHP, "r") as shapefile:
+        features = [feature["geometry"] for feature in shapefile]
+    with rasterio.open(InputTIF) as src:
+        out_image, out_transform = rasterio.mask.mask(src, features, crop=True)
+        out_meta = src.meta.copy()
+    out_meta.update({"driver": "GTiff",
+                 "height": out_image.shape[1],
+                 "width": out_image.shape[2],
+                 "transform": out_transform})
+    with rasterio.open(OutTIF, "w", **out_meta) as dest:
+        dest.write(out_image)
