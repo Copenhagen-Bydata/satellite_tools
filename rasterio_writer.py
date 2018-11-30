22d30c3afaee4906c804c0f69b29a01e97739cf7
Emil Møller Rasmussen <35135818+hy42-tmf@users.noreply.github.com>
2018-11-30 13:35:26 +0100

    Update rasterio_writer.py

--- rasterio_writer.py
+++ rasterio_writer.py
@@ -1,5 +1,6 @@
-# data must be a valid NumPy array
+import rasterio
 
+# data must be a valid NumPy array
 def rasterio_writer(outputLocation, data, metadata):
   with rasterio.open(outputLocation, 'w', **metadata) as dest:
 		dest.write(data, 1)
