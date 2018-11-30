2474db4d62f1c25062b3ea52ffaa1484c6181cc7
Emil Møller Rasmussen <35135818+hy42-tmf@users.noreply.github.com>
2018-11-30 14:36:19 +0100

    Update rasterio_mask.py

--- rasterio_mask.py
+++ rasterio_mask.py
@@ -1,10 +1,13 @@
 import fiona, rasterio, gdal, rasterio.mask
 
+#
+# https://rasterio.readthedocs.io/en/latest/api/rasterio.mask.html
+# 
 def clipper(InputTIF, InputSHP, OutTIF):
     with fiona.open(InputSHP, "r") as shapefile:
         features = [feature["geometry"] for feature in shapefile]
     with rasterio.open(InputTIF) as src:
-        out_image, out_transform = rasterio.mask.mask(src, features, crop=True)
+        out_image, out_transform = rasterio.mask.mask(src, features, invert=True)
         out_meta = src.meta.copy()
     out_meta.update({"driver": "GTiff",
                  "height": out_image.shape[1],
